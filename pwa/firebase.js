df205b51c7bf472dd6e0fb28cb82c50fb3808f97
Earthman Topraksoy 土本 الترابي <70323394+TopraksoyEarthmanTsuchimoto@users.noreply.github.com>
2024-09-06 05:04:27 +0300

    Update firebase.js

--- pwa/firebase.js
+++ pwa/firebase.js
@@ -18,6 +18,19 @@ const firebaseConfig = {
 
 const tokenPart1 = "9555ec"; const tokenPart2 = "0b30003e"; // ipinfo.io access
 
+function getPresentDate() {
+  const currentDate = new Date();
+  const year = currentDate.getFullYear();
+  const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  const month = monthNames[currentDate.getMonth()];
+  const day = String(currentDate.getDate()).padStart(2, '0');
+  const hour = String(currentDate.getHours()).padStart(2, '0');
+  const minute = String(currentDate.getMinutes()).padStart(2, '0');
+  const second = String(currentDate.getSeconds()).padStart(2, '0');
+  const formattedDateTime = `${year}-${month}-${day}|${hour}:${minute}:${second}`; //console.log(formattedDateTime);
+  return formattedDateTime;
+}
+
 window.addEventListener("load",checkLocalStorageToHandleData,{once:true});
 function checkLocalStorageToHandleData() {
   if (localStorage.getItem("thisUserIsAlreadyCounted")) {
@@ -27,14 +40,13 @@ function checkLocalStorageToHandleData() {
       const app = initializeApp(firebaseConfig);
       const db = getDatabase(app);
 
-      let jsonResponse = {ip: "?.?.?.?",country:"?",region:"?",city:"?"};
+      let jsonResponse1 = {ip: "?.?.?.?",country:"?",region:"?",city:"?"};
       setTimeout(function () {
         /* Before September 2024
         const apiURL = 'https://api.ipify.org?format=json';
         let response; let data; let aypii="0.0.0.0";
         */
-
-        // const user_agent_string = navigator.userAgent; // Use clean strings from ua-parser-js
+        // DEPRECATED: const user_agent_string = navigator.userAgent; // Use clean strings from ua-parser-js instead
         // Function to get IP address
         async function getIPAddress() {
             try {
@@ -43,29 +55,19 @@ function checkLocalStorageToHandleData() {
                 data = await response.json();
                 aypii = data.ip; // NOT CERTAIN what happens when the IP is hidden
                 */
-                const request = await fetch("https://ipinfo.io/json?token="+tokenPart1+tokenPart2);
-                jsonResponse = await request.json();
-                // console.log(jsonResponse.ip, jsonResponse.country,jsonResponse.region,jsonResponse.city) // WORKS OK
+                const request1 = await fetch("https://ipinfo.io/json?token="+tokenPart1+tokenPart2);
+                jsonResponse1 = await request1.json();
             } catch (error) {
                 console.error('Error fetching IP address:', error);
             } finally {
-                const currentDate = new Date();
-                const year = currentDate.getFullYear();
-                const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-                const month = monthNames[currentDate.getMonth()];
-                const day = String(currentDate.getDate()).padStart(2, '0');
-                const hour = String(currentDate.getHours()).padStart(2, '0');
-                const minute = String(currentDate.getMinutes()).padStart(2, '0');
-                const second = String(currentDate.getSeconds()).padStart(2, '0');
-                const formattedDateTime = `${year}-${month}-${day}|${hour}:${minute}:${second}`; //console.log(formattedDateTime);
                 // Write to firebase realtime database regardlessly, either if IP check succeeds or fails
-                set(ref(db, "New users from "+jsonResponse.country+"/"+jsonResponse.region+"/"+jsonResponse.city+" at "+formattedDateTime), {
+                set(ref(db, "New users from "+jsonResponse1.country+"/"+jsonResponse1.region+"/"+jsonResponse1.city+" at "+getPresentDate()), {
                   /* Before September 2024
                   user_interface: userInterfaceLanguage,
                   ip: aypii,
                   ua_string: user_agent_string*/
                   appDomain: location.href,
-                  eyepea: jsonResponse.ip,
+                  eyepea: jsonResponse1.ip,
                   using: detectedBrowserName+" on "+detectedOS_name+" using a "+detectedBrandName+" "+deviceDetector.device // See js_for_different_browsers_and_devices
                 }).then(()=>{
                   console.log("User visit saved"); // Works OK
@@ -93,7 +95,7 @@ export async function savePotentialDonor() { // See information.js
   } catch (error) {
       console.error('Error fetching IP address:', error);
   } finally {
-    set(ref(db, "Possible donor from "+jsonResponse2.country+"/"+jsonResponse2.region+"/"+jsonResponse2.city), {
+    set(ref(db, "Possible donor(s) from "+jsonResponse2.country+"/"+jsonResponse2.region+"/"+jsonResponse2.city+" at "+getPresentDate()), {
       appDomain: location.href,
       eyepea: jsonResponse2.ip,
       using: detectedBrowserName+" on "+detectedOS_name+" using a "+detectedBrandName+" "+deviceDetector.device // See js_for_different_browsers_and_devices
